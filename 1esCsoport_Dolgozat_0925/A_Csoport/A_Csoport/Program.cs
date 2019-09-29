5ddc651303f0da173126d5dad72d9a0cb156065a
Horváth Dániel <admin@hdani1337.hu>
2019-09-29 15:00:00 +0200

    Első csoport dolgozata 09.25.-én

---
+++ 1esCsoport_Dolgozat_0925/A_Csoport/A_Csoport/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Csoport
+{
+    class Program
+    {
+        Program()
+        {
+            szamok();//Első feladat
+            betuk();//Második feladat
+            bonusz();//:DDDDD
+        }
+
+        void szamok()
+        {
+            int[] szamjegyek = new int[25];//25 elemű tömb a 25 számjegynek
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Adjon meg egy 25 számjegyű számot: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string szam = Console.ReadLine();//Stringben vesszük fel, mert túl hosszú lenne számnak (lásd long.MaxValue)
+
+            if (szam.Length != 25)
+            {//Hogyha a szám nem 25 számjegyű, kérjünk be újat
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A szám nem 25 számjegyű! Újra!");
+                szamok();
+            }
+            else
+            {//Ha 25 számjegyű
+                for (int i = 0; i < 25; i++)
+                {//Akkor töltsük fel a tömböt a számjegyekkel
+                    szamjegyek[i] = Convert.ToInt32(szam[i]);//A String i-dik elemét számmá alakítjuk, ha mégsem számjegy az i-dik elem, az 0-t fog visszaadni
+                }
+
+                int parosSzamok = 0;
+                int paratlanSzamok = 0;
+                for (int i = 0; i < szamjegyek.Length; i++)
+                {//Megszámoljuk mennyi páros és páratlan szám van
+                    if (szamjegyek[i] % 2 == 0) parosSzamok++;
+                    else paratlanSzamok++;
+                }
+                Console.WriteLine("A páros számok aránya " + (parosSzamok / 25.0 * 100) + "%.");//Visszaadjuk a páros számok arányát százalékban
+            }
+        }
+
+        void betuk()
+        {
+            int[] userNumbers = new int[5];//A felhasználó számai
+            int currentSzam;//Éppen megadott szám
+            int index = 0;
+            int mennyiOlyan = 0;//A feltételnek megfelelő számok mennyisége
+            Console.WriteLine("Kérem írjon be 5 darab kétjegyű számot!");
+
+            while (userNumbers[4] == 0)
+            {//Amíg nincs meg az 5 szám (mivel ilyenkor a tömbnek az üres elemei 0)
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("Ide írja a számot: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Int32.TryParse(Console.ReadLine(), out currentSzam);
+                if (currentSzam < 10 || currentSzam > 99)
+                {//Ha a szám nem kétjegyű
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("A szám nem tartozik bele a megadott intervallumba, újra!");
+                }
+                else userNumbers[index++] = currentSzam;//A kétjegyű számokat belerakjuk a tömbbe                
+            }
+
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                if (userNumbers[i] % 10 == 1 || userNumbers[i] % 10 == 3 || userNumbers[i] % 10 == 5) mennyiOlyan++;//Végződés
+                else if (userNumbers[i] / 10 == 1 || userNumbers[i] / 10 == 3 || userNumbers[i] / 10 == 5) mennyiOlyan++;//Kezdődés
+                //Egy feltételbe is berakhattam volna őket, de így átláthatóbb talán
+            }
+
+            Console.WriteLine(mennyiOlyan + " darab számban szerepelnek a megadott számok.");
+        }
+
+        void bonusz()
+        {//Ciklusok segítségével írtam ki :DDDDDDDD    
+            //Most itthon megtudnám csinálni de cseszett lusta vagyok, megoldást lásd master/Matrix
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.WriteLine("1 1 1 1 1 1 1 1 1 1\n" +
+                        "1 X 0 0 0 0 0 0 M 1\n" +
+                        "1 0 X 0 0 0 0 M 0 1\n" +
+                        "1 0 0 X 0 0 M 0 0 1\n" +
+                        "1 0 0 0 X M 0 0 0 1\n" +
+                        "1 0 0 0 0 X 0 0 0 1\n" +
+                        "1 0 0 0 0 0 X 0 0 1\n" +
+                        "1 0 0 0 0 0 0 X 0 1\n" +
+                        "1 0 0 0 0 0 0 0 X 1\n" +
+                        "1 1 1 1 1 1 1 1 1 1");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Horváth Dániel Programozás Gyakorlat Dolgozat - A. csoport";
+            new Program();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1esCsoport_Dolgozat_0925/B_Csoport/B_Csoport/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Csoport
+{
+    class Program
+    {
+
+        Program()
+        {
+            primek();
+            szamok();
+        }
+
+        Random random = new Random();
+
+        void primek()
+        {
+            int[] szamok = new int[10000];//16 giga ramom nem lesz elég ekkora tömbhöz :D 
+            int[] egyediek = new int[10000];
+            int egyediekIndex = 0;
+            int mennyiPrim = 0;
+            int mennyiEgyedi = 0;
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = random.Next(1, 10000);
+                if (isPrim(szamok[i])) mennyiPrim++;
+                if (!egyediek.Contains(szamok[i]))
+                {
+                    egyediek[egyediekIndex++] = szamok[i];
+                    mennyiEgyedi++;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("A random számok közül " + mennyiPrim + " darab volt prímszám, illetve " + mennyiEgyedi + " volt egyedi.");
+        }
+
+        bool isPrim(int szam)
+        {
+            for (int i = 2; i < szam; i++)
+            {
+                if (szam % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        void szamok()
+        {
+            int[] tomb1 = new int[random.Next(10, 20)];
+            int[] egyezoek = new int[tomb1.Length];
+            int mennyiEgyezo = 0;
+            int egyezoIndex = 0;
+
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                tomb1[i] = random.Next(10, 99);
+            }
+
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                if (tomb1[i] % 10 == tomb1[i] / 10) mennyiEgyezo++;
+            }
+
+            egyezoek = new int[mennyiEgyezo];
+
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                if (tomb1[i] % 10 == tomb1[i] / 10) egyezoek[egyezoIndex++] = tomb1[i];
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            if (egyezoek.Length > 0)
+            {
+
+                Console.Write("A feltételnek megegyező számok a következőek: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                for (int i = 0; i < egyezoek.Length; i++)
+                {
+                    if (i < egyezoek.Length - 1) Console.Write(egyezoek[i] + ", ");
+                    else Console.Write(egyezoek[i] + ".");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A feltételnek egy szám sem felelt meg.");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("A tömb legkisebb eleme a(z) " + tomb1.Min() + ", a legnagyobb pedig a(z) " + tomb1.Max() + ".");
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Nyomj egy gombot a kilépéshez...");
+            Console.ReadKey();
+        }
+    }
+}
