f8d9b6fe60bc8f6d8a5d0bfd1ae7be17cb158868
Horváth Dániel <admin@hdani1337.hu>
2019-11-11 12:02:06 +0100

    Színkezelés

--- GUI/GUI/Form1.Designer.cs
+++ GUI/GUI/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.buttonExit = new System.Windows.Forms.Button();
+            this.vscRed = new System.Windows.Forms.VScrollBar();
+            this.vscBlue = new System.Windows.Forms.VScrollBar();
+            this.vscGreen = new System.Windows.Forms.VScrollBar();
+            this.lRed = new System.Windows.Forms.Label();
+            this.lGreen = new System.Windows.Forms.Label();
+            this.lBlue = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonExit
@@ -36,15 +42,81 @@
             this.buttonExit.BackColor = System.Drawing.Color.Maroon;
             this.buttonExit.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonExit.Font = new System.Drawing.Font("Comic Sans MS", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonExit.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold);
             this.buttonExit.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.buttonExit.Location = new System.Drawing.Point(0, 172);
+            this.buttonExit.Location = new System.Drawing.Point(0, 223);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(624, 149);
+            this.buttonExit.Size = new System.Drawing.Size(624, 98);
             this.buttonExit.TabIndex = 0;
             this.buttonExit.Text = "Exit";
             this.buttonExit.UseVisualStyleBackColor = false;
             this.buttonExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vscRed
+            // 
+            this.vscRed.Location = new System.Drawing.Point(321, -1);
+            this.vscRed.Maximum = 264;
+            this.vscRed.Name = "vscRed";
+            this.vscRed.Size = new System.Drawing.Size(18, 221);
+            this.vscRed.TabIndex = 1;
+            this.vscRed.Value = 255;
+            this.vscRed.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vscRed_Scroll_1);
+            // 
+            // vscBlue
+            // 
+            this.vscBlue.Location = new System.Drawing.Point(9, -1);
+            this.vscBlue.Maximum = 264;
+            this.vscBlue.Name = "vscBlue";
+            this.vscBlue.Size = new System.Drawing.Size(17, 221);
+            this.vscBlue.TabIndex = 2;
+            this.vscBlue.Value = 255;
+            this.vscBlue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vscRed_Scroll);
+            // 
+            // vscGreen
+            // 
+            this.vscGreen.Location = new System.Drawing.Point(163, -1);
+            this.vscGreen.Maximum = 264;
+            this.vscGreen.Name = "vscGreen";
+            this.vscGreen.Size = new System.Drawing.Size(17, 221);
+            this.vscGreen.TabIndex = 3;
+            this.vscGreen.Value = 255;
+            this.vscGreen.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vscGreen_Scroll);
+            // 
+            // lRed
+            // 
+            this.lRed.AutoSize = true;
+            this.lRed.BackColor = System.Drawing.Color.Black;
+            this.lRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lRed.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lRed.Location = new System.Drawing.Point(29, 103);
+            this.lRed.Name = "lRed";
+            this.lRed.Size = new System.Drawing.Size(58, 29);
+            this.lRed.TabIndex = 4;
+            this.lRed.Text = "Red";
+            // 
+            // lGreen
+            // 
+            this.lGreen.AutoSize = true;
+            this.lGreen.BackColor = System.Drawing.Color.Black;
+            this.lGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lGreen.ForeColor = System.Drawing.SystemColors.Control;
+            this.lGreen.Location = new System.Drawing.Point(183, 103);
+            this.lGreen.Name = "lGreen";
+            this.lGreen.Size = new System.Drawing.Size(80, 29);
+            this.lGreen.TabIndex = 5;
+            this.lGreen.Text = "Green";
+            // 
+            // lBlue
+            // 
+            this.lBlue.AutoSize = true;
+            this.lBlue.BackColor = System.Drawing.Color.Black;
+            this.lBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lBlue.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lBlue.Location = new System.Drawing.Point(342, 103);
+            this.lBlue.Name = "lBlue";
+            this.lBlue.Size = new System.Drawing.Size(62, 29);
+            this.lBlue.TabIndex = 6;
+            this.lBlue.Text = "Blue";
             // 
             // formMain
             // 
@@ -52,17 +124,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.lBlue);
+            this.Controls.Add(this.lGreen);
+            this.Controls.Add(this.lRed);
+            this.Controls.Add(this.vscGreen);
+            this.Controls.Add(this.vscBlue);
+            this.Controls.Add(this.vscRed);
             this.Controls.Add(this.buttonExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "formMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.formMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.VScrollBar vscRed;
+        private System.Windows.Forms.VScrollBar vscBlue;
+        private System.Windows.Forms.VScrollBar vscGreen;
+        private System.Windows.Forms.Label lRed;
+        private System.Windows.Forms.Label lGreen;
+        private System.Windows.Forms.Label lBlue;
     }
 }
 
--- GUI/GUI/Form1.cs
+++ GUI/GUI/Form1.cs
@@ -12,6 +12,8 @@ namespace GUI
 {
     public partial class formMain : Form
     {
+        int alpha, red, green, blue = 0;
+
         public formMain()
         {
             InitializeComponent();
@@ -22,5 +24,38 @@ namespace GUI
             Close();
         }
 
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            lRed.Text = "Red: 0";
+            lBlue.Text = "Blue: 0";
+            lGreen.Text = "Green: 0";
+            this.BackColor = Color.Black;
+        }
+
+        private void vscRed_Scroll(object sender, ScrollEventArgs e)
+        {
+            red = (255 - e.NewValue);            
+            update();
+        }
+
+        private void vscGreen_Scroll(object sender, ScrollEventArgs e)
+        {
+            green = (255 - e.NewValue);            
+            update();
+        }
+
+        private void vscRed_Scroll_1(object sender, ScrollEventArgs e)
+        {
+            blue = (255 - e.NewValue);          
+            update();
+        }
+
+        private void update()
+        {
+            this.BackColor = Color.FromArgb(red, green, blue);
+            lBlue.Text = "Blue: " + blue;
+            lGreen.Text = "Green: " + green;
+            lRed.Text = "Red: " + red;
+        }
     }
 }
