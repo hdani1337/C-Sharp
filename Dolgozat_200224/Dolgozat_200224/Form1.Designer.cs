f2d3e7ebe07680b8abf9c1c0fccbf206dfc3d2d1
Horváth Dániel <admin@hdani1337.hu>
2020-02-24 11:32:39 +0100

    Login formos dolgozat

---
+++ Dolgozat_200224/Dolgozat_200224/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Dolgozat_200224
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.passValue = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.passLabel = new System.Windows.Forms.Label();
+            this.nameValue = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel2.BackgroundImage")));
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.loginButton, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(683, 392);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tableLayoutPanel1.BackgroundImage")));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.63397F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.36603F));
+            this.tableLayoutPanel1.Controls.Add(this.passValue, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.passLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.nameValue, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(677, 190);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // passValue
+            // 
+            this.passValue.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.passValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passValue.Location = new System.Drawing.Point(339, 127);
+            this.passValue.Name = "passValue";
+            this.passValue.PasswordChar = '*';
+            this.passValue.Size = new System.Drawing.Size(335, 31);
+            this.passValue.TabIndex = 3;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.nameLabel.Location = new System.Drawing.Point(223, 20);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(110, 55);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Név";
+            // 
+            // passLabel
+            // 
+            this.passLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.passLabel.AutoSize = true;
+            this.passLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.passLabel.Location = new System.Drawing.Point(173, 115);
+            this.passLabel.Name = "passLabel";
+            this.passLabel.Size = new System.Drawing.Size(160, 55);
+            this.passLabel.TabIndex = 1;
+            this.passLabel.Text = "Jelszó";
+            // 
+            // nameValue
+            // 
+            this.nameValue.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.nameValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameValue.Location = new System.Drawing.Point(339, 32);
+            this.nameValue.Name = "nameValue";
+            this.nameValue.Size = new System.Drawing.Size(335, 31);
+            this.nameValue.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.Green;
+            this.loginButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loginButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.loginButton.Location = new System.Drawing.Point(3, 199);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(677, 190);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Belépés";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(683, 392);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox passValue;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.TextBox nameValue;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ Dolgozat_200224/Dolgozat_200224/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dolgozat_200224
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Nev;
+        public string Jelszo;
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            //Klikk
+
+            //Felhasználónév vizsgálata, 3 karakternél hosszabbnak kell lennie
+            if (nameValue.Text.Length <= 3) MessageBox.Show("A név túl rövid!");
+
+            //Ha jó a felhasználónév akkor folytatjuk a vizsgálást
+            else {
+                Nev = nameValue.Text;
+
+                //Jelszó vizsgálata, 6 karakternek kell lennie
+                if (passValue.Text.Length != 6) MessageBox.Show("A jelszó nem 6 karakter hosszú!");
+                else
+                {
+                    Jelszo = passValue.Text;
+                    //Ha megvan 6 karakter, tovább vizsgáljuk
+
+                    //3 számnak kell lennie benne, 2 betűnek és egy különleges karakternek
+
+                    int szamok = 0;
+                    int betuk = 0;
+                    int kulonleges = 0;
+
+                    int tempNum = 0;
+
+                    for (int i = 0; i < Jelszo.Length; i++)
+                    {
+                        Int32.TryParse(Jelszo[i] + "", out tempNum);
+
+                        if (tempNum != 0)
+                        {//A vizsgált karakter egy szám
+                            szamok++;
+                            tempNum = 0;
+                        }
+                        else
+                        {//A vizsgált karakter egy betű
+                            if (Jelszo[i].Equals('?') || Jelszo[i].Equals('.') || Jelszo[i].Equals('!') || Jelszo[i].Equals(':') || Jelszo[i].Equals(';'))
+                            {
+                                //Ha különleges karakter
+                                kulonleges++;
+                            }
+                            else
+                            {
+                                //Ha egy közönséges betű
+                                betuk++;
+                            }
+                        }
+
+                    }
+
+                    //Karakterválogatás kész
+
+                    if (betuk == 2 && szamok == 3 && kulonleges == 1)
+                    {
+                        //Jó jelszó
+                        MessageBox.Show("Felhasználónév és jelszó elmentve a belep.txt fájlba");
+
+                        File.AppendAllText("belep.txt", "Felh: " + Nev + "; Jel: " + Jelszo + "\n");
+                    }
+                    else
+                    {
+                        //Rossz jelszó
+
+                        string error = "Kell még ";
+                        error += (2 - betuk) + " darab betű, ";
+                        error += (3 - szamok) + " darab szám, ";
+                        error += "egy darab különleges karakter.";
+
+                        MessageBox.Show("Nem megfelelő jelszó!\nBetűk száma: " + betuk + "\nSzámok száma: " + szamok + "\nKülönleges karakterek száma: " + kulonleges + "\n" + error);
+                    }
+                }
+            }        
+        }
+    }
+}
